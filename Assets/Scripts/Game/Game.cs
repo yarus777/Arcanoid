5f8fff0ce6dc3b1562e81b305f5a0aabef28142e
Alex Gorbach <aleks.gorbach@gmail.com>
2015-05-12 21:44:33 +0300

    GameScene added

---
+++ Assets/Scripts/Game/Game.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game {
+    class Game : MonoBehaviour {
+        public static Game Instance { get; private set; }
+
+        private void Awake() {
+            Instance = this;
+        }
+    }
+}
