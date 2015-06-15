cf0b80ef23677579b8ed0aca946555d2bf0505fc
Yaroslava Rusakova <yarus7777@gmail.com>
2015-06-15 11:23:48 +0300

    Decrease platform bonus

---
+++ Assets/Scripts/Game/Bonuses/Implementations/SmallPlatformBonus.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Assets.Scripts.Game.Bonuses.Implementations
+{
+    class SmallPlatformBonus : CollectableBonus
+
+    {
+        public override void Apply()
+        {
+            Game.Instance.Player.DecreaseSize();
+        }
+    }
+}
--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -5,6 +5,7 @@ namespace Assets.Scripts.Game.Player {
     public class Player : MonoBehaviour {
         public const float SIZE_DELTA = 50;
         public const float MAX_SIZE = 300;
+        public const float MIN_SIZE = 25;
 
         private RectTransform _transform;
 
@@ -15,6 +16,16 @@ namespace Assets.Scripts.Game.Player {
         public void Increase() {
             var size = Transform.sizeDelta + new Vector2(SIZE_DELTA, 0);
             if (size.x > MAX_SIZE) {
+                return;
+            }
+            Transform.DOSizeDelta(size, 0.7f);
+        }
+
+        public void Decrease()
+        {
+            var size = Transform.sizeDelta - new Vector2(SIZE_DELTA, 0);
+            if (size.x < MIN_SIZE)
+            {
                 return;
             }
             Transform.DOSizeDelta(size, 0.7f);
